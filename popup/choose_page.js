374f4bb4a955322b4d7cd457a047a3a6e183e7cf
Shaon Majumder <shaon.fdr@robist.com>
2019-04-21 07:19:36 +0600

    dynamic select

--- popup/choose_page.js
+++ popup/choose_page.js
@@ -1,10 +1,14 @@
-var postUrl = "http://localhost/chrome_content_saver/popup/test.php";
+var postUrl = "http://localhost/chrome_content_saver/popup/post.php";
 
-function generate_valid_key(){  
+function random_string(num_s){
+  let r = Math.random().toString(36).substring(num_s);
+  return r;
+}
+
+function generate_valid_key(){
   var con = true;
   while(con){
-    let r = Math.random().toString(36).substring(7);
-    var key_need = r;
+    var key_need = random_string(7);
     $.ajax({
       type: 'POST',
       url: postUrl,
@@ -74,19 +78,76 @@ function getCurrentTabUrls(){
   
 }
 
+function remove_select_option_by_value(id,value){
+  var selectobject=document.getElementById(id);
+  for (var i=0; i<selectobject.length; i++){
+  if (selectobject.options[i].value == value )
+     selectobject.remove(i);
+  }
+}
+
+
+
+  
+
+
+function populate_categories(){
+  if(!document.getElementById("select")){
+    var select_div = document.createElement("select");
+    select_div.setAttribute("id", "select");
+    select_div.onchange = function () {
+      if (this[this.selectedIndex].value === "NEW") {      
+        remove_select_option_by_value("select","NEW");
+
+        var input_div = document.createElement("div");
+        input_div.setAttribute("id", "input_cat_div");
+        document.body.appendChild(input_div);
+        input_div.innerHTML += '<input type="text" id="new_input_cat" placeholder="New Category"><input id="new_input_cat_submit" type="submit">';
+      }
+    };
+
+    document.getElementById("form1").insertBefore(select_div, document.getElementById("form1").firstChild);
+  }else{
+    var select_div = document.getElementById("select");
+    select_div.onchange = function () {
+      if (this[this.selectedIndex].value === "NEW") {      
+        remove_select_option_by_value("select","NEW");
+
+        var input_div = document.createElement("div");
+        input_div.setAttribute("id", "input_cat_div");
+        document.body.appendChild(input_div);
+        input_div.innerHTML += '<input type="text" id="new_input_cat" placeholder="New Category"><input id="new_input_cat_submit" type="submit">';
+      }
+    };
+  }
+
+  $.post(postUrl, {"action":"get_cats"}, function(txt){
+    if(select_div){
+      var arTxt = txt.split(',');
+      for (let cat of arTxt) {
+        select_div.innerHTML += '<option value="'+cat+'">'+cat+'</option>';
+      }
+      select_div.innerHTML += '<option value="NEW">NEW</option>';  
+    }
+    
+  });
+}
+
 document.addEventListener("click", function(e) {
   //page 1
   if(e.target.id == "submit_to_category"){
-    $.post(postUrl, {"action":"get_cats"}, function(txt){
-      var arTxt = txt.split(',');
-      for (let cat of arTxt) {
-        document.getElementById("select").innerHTML += '<option value="'+cat+'">'+cat+'</option>';
-      }
-    });
     document.getElementById("initial").style="display:none;";
     document.getElementById("form1").style="display:block;";
+    populate_categories();
   }
+  else if(e.target.id == "new_input_cat_submit"){    
+    var inText = document.getElementById("new_input_cat");
+    document.getElementById("select").innerHTML += '<option value="'+inText.value+'">'+inText.value+'</option>';
+    document.getElementById("select").innerHTML += '<option value="NEW">NEW</option>';
 
+    var elem = document.getElementById("input_cat_div");
+    elem.parentNode.removeChild(elem);
+  }
   else if(e.target.id == "restore_last_session"){
     $.ajax({
       type: 'POST',
@@ -133,39 +194,38 @@ document.addEventListener("click", function(e) {
           urls.push(tab.url);
         }
         
-        var r = generate_valid_key();
+        
         $.ajax({
           type: 'POST',
           url: postUrl,
-          data: {"action":"save_this_session","urls":urls.join(','),"postkey":r},
+          data: {"action":"save_this_session","urls":urls.join(','),"postkey":generate_valid_key()},
           error: function(jqXHR, textStatus) { alert(textStatus); },
           dataType: 'text',
           async: false,
           success: function(txt){
-                if(txt == r){
-                  document.body.innerHTML = "Saved your tabs !!!";
-                  setTimeout(function(){ window.close(); }, 1500);
-                  chrome.tabs.query(
-                  {/*currentWindow: true, active: true*/}, 
-                    function(tabs){
-                      for(let tab of tabs){
-                        chrome.tabs.remove(tab.id);
-                        //last tab remains when it closes so it can add up this tab when restored
-                      }
-                    }
-                  );
-                }else{
-                  alert("All this tabs are already saved !!!");
+            if(txt == r){
+              document.body.innerHTML = "Saved your tabs !!!";
+              setTimeout(function(){ window.close(); }, 1500);
+              chrome.tabs.query(
+              {/*currentWindow: true, active: true*/}, 
+                function(tabs){
+                  for(let tab of tabs){
+                    chrome.tabs.remove(tab.id);
+                    //last tab remains when it closes so it can add up this tab when restored
+                  }
                 }
+              );
+            }else{
+              alert("All this tabs are already saved !!!");
+            }
           }
         });
       }
     );
   }
   else if(e.target.id == "export_opened_tab_urls"){
-     //var TagName_ = window.prompt("Enter a file URL","TagName");
+    //var TagName_ = window.prompt("Enter a file URL","TagName");
     getCurrentTabUrls();
-    
   }
   //page 1
 
@@ -174,55 +234,17 @@ document.addEventListener("click", function(e) {
     chrome.tabs.query(
     {currentWindow: true, active: true}, 
       function(tabs){
-            
             var title1 = tabs[0].title;
             var url1 = tabs[0].url;
             //var TagName_ = window.prompt("Enter a file URL","TagName");
             var TagName_ = $("#select :selected").val();
             var solution_ = escape($("#solution").val());
             //use unescape() to read after getting data from database
-            var postkey = "2343";
-              
-            var con = true;
-            while(con){
-              let r = Math.random().toString(36).substring(7);
-              var key_need = r;
-              
-              $.ajax({
-                      type: 'POST',
-                      url: postUrl,
-                      data: {"action":"validate_key","key_need":key_need},
-                      error: function(jqXHR, textStatus) { alert(textStatus); },
-                      dataType: 'text',
-                      async: false,
-                      success: function(txt){
-                                if(txt == "No Match key"){
-                                  //alert("Unique");
-                                  con = false;    // breaking loop
-                                  
-                                }else if(key_need == txt){
-                                  //alert("Not Unique");
-                                }
-                              }
-              });
-
-              /*$.post(postUrl, {"key_need":key_need}, function(txt){
-                if(txt == "No Match key"){
-                  alert("Unique");
-                  con = false;   // not in work 
-                  //close tab
-                }else if(key_need == txt){
-                  alert("Not Unique");
-                }
-              });*/
-              
-            }
-            //alert("Achieved: "+key_need);
             var data = {
                 "title": title1,
                 "url": url1,
                 "tag": TagName_,
-                "postkey": key_need,
+                "postkey": generate_valid_key(),
                 "solution":solution_,
                 "action":"post_data"
             };  
